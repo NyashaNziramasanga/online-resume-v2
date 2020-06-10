fa14c672f91accac2305fd82faa85063f3f66fc9
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-06-10 21:53:28 +0930

    Refactored Buttin, paragraph and wakatime

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,16 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Styles from './Button.module.scss';
 
 export const Button = (props) => {
   return (
-    <Fragment>
-      <a
-        className={Styles.button}
-        href={props.URL}
-        download="resume-of-nyasha-nash-nziramasanga.pdf"
-      >
-        <div className={Styles.buttonLink}>PDF of my full resume</div>
-      </a>
-    </Fragment>
+    <a
+      className={Styles.button}
+      href={props.URL}
+      download="resume-of-nyasha-nash-nziramasanga.pdf"
+    >
+      <div className={Styles.buttonLink}>PDF of my full resume</div>
+    </a>
   );
 };
--- src/components/UI/Paragraph/paragraph.jsx
+++ src/components/UI/Paragraph/paragraph.jsx
@@ -2,9 +2,5 @@ import React from 'react';
 import Styles from './Paragraph.module.scss';
 
 export const Paragraph = (props) => {
-  return (
-    <div>
-      <p className={Styles.paragraph}>{props.content}</p>
-    </div>
-  );
+  return <p className={Styles.paragraph}>{props.content}</p>;
 };
